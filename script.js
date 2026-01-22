ba77f74354f44683e1a3fe589a690939466c36e4
user2009487 <ssathish4743@gmail.com>
2026-01-22 11:53:40 -0800

    js change

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
-//selectin popup box popup overlay button
-var popupoverlay=document.querySelector(".popup-overlay")
-var popupbox=document.querySelector(".popup-box")
-var addpopupbutton=document.getElementById("add-popup-button")
+// selecting popup box, popup overlay, add button
+var popupoverlay = document.querySelector(".popup-overlay")
+var popupbox = document.querySelector(".popup-box")
+var addpopupbutton = document.getElementById("add-popup-button")
 
-addpopupbutton.addEventListener("click",function(){
-    popupoverlay.style.display="block"
-    popupbox.style.display="block"
+addpopupbutton.addEventListener("click", function () {
+    popupoverlay.style.display = "block"
+    popupbox.style.display = "block"
 })
 
-
-//select cancel button
-var cancelpopup=document.getElementById("cancel-popup")
-cancelpopup.addEventListener("click",function(event){
+// selecting cancel button
+var cancelpopup = document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click", function (event) {
     event.preventDefault()
-    popupoverlay.style.display="none"
-    popupbox.style.display="none"
+    popupoverlay.style.display = "none"
+    popupbox.style.display = "none"
 })
 
-//select container,add-book,book-title-input,book-author-input,book-description-input
+// selecting container and inputs
+var container = document.querySelector(".container")
+var addbook = document.getElementById("add-book")
+var booktitleinput = document.getElementById("book-title-input")
+var bookauthorinput = document.getElementById("book-author-input")
+var bookdescriptioninput = document.getElementById("book-description-input")
 
-var container=document.querySelector(".container")
-var addbook=document.getElementById("add-book")
-var booktitleinput=document.getElementById("book-title-input")
-var bookauthorinput=document.getElementById("book-author-input")
-var bookdescriptioninput=document.getElementById("book-description-input")
-
-addbook.addEventListener("click",function(event){
+// ADD BOOK
+addbook.addEventListener("click", function (event) {
     event.preventDefault()
-    var div=document.createElement("div")
-    div.setAttribute("class","book-container")
-    div.innerHTML=` <h2>${booktitleinput.value}</h2>
-            <h5>${bookauthorinput.value}</h5>
-            <p>${bookdescriptioninput.value}</p>
-            <button onclick="deletebook(event)">Delete</button>`
-             container.append(div)
-             popupoverlay.style.display="none"
-             popupbox.style.display="none"
 
+    var div = document.createElement("div")
+    div.setAttribute("class", "book-container")
+    div.innerHTML = `
+        <h2>${booktitleinput.value}</h2>
+        <h5>${bookauthorinput.value}</h5>
+        <p>${bookdescriptioninput.value}</p>
+        <button onclick="deletebook(event)">Delete</button>
+    `
+
+    container.append(div)
+
+    // clear inputs
+    booktitleinput.value = ""
+    bookauthorinput.value = ""
+    bookdescriptioninput.value = ""
+
+    popupoverlay.style.display = "none"
+    popupbox.style.display = "none"
+
+    saveBooksToStorage()
 })
 
-function deletebook(event){
+// DELETE BOOK
+function deletebook(event) {
     event.target.parentElement.remove()
+    saveBooksToStorage()
+}
+
+// SAVE TO localStorage
+function saveBooksToStorage() {
+    localStorage.setItem("books", container.innerHTML)
+}
+
+// LOAD FROM localStorage ON PAGE LOAD
+window.onload = function () {
+    var savedBooks = localStorage.getItem("books")
+    if (savedBooks) {
+        container.innerHTML = savedBooks
+    }
 }
